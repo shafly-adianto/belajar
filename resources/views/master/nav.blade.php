c14707c4fcceffdf3aef004a96fc2bc85391ec9a
irfan23m <51937369+irfan23m@users.noreply.github.com>
2019-11-17 23:30:01 +0700

    last Commit

--- resources/views/master/nav.blade.php
+++ resources/views/master/nav.blade.php
@@ -10,9 +10,16 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="{{url('')}}/produk" class="{{ Request::is('*produk*') ? 'active' : '' }}">Produk</a></li>
-        <li><a href="{{url('')}}/simulasi" class="{{ Request::is('*simulasi*') ? 'active' : '' }}">Simulasi</a></li>
-        <li><a href="{{url('')}}/info_harga_pasar" class="{{ Request::is('*info_harga_pasar*') ? 'active' : '' }}">Info Harga Pasar</a></li>
+        <li><a href="{{url('')}}/produk" class="{{ Request::is('produk') ? 'active' : '' }}">Produk</a></li>
+        <li>
+          <a class="dropdown-toggle" data-toggle="dropdown" class="{{ Request::is('produk') ? 'active' : '' }}" href="#">Simulasi</a>
+          <ul class="dropdown-menu">
+            <li><a href="/simulasiGadai">Simulasi Gadai</a></li>
+            <li><a href="/simulasiBeliEmas">Simulasi Beli Emas</a></li>
+            <li><a href="/simulasiKreditAmanah">Simulasi Kredit Amanah</a></li>
+          </ul>
+        </li>
+        <li><a href="{{url('')}}/info_harga_pasar" class="{{ Request::is('info_harga_pasar') ? 'active' : '' }}">Info Harga Pasar</a></li>
       </ul>
     </div>
   </div>
